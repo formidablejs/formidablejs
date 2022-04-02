354b0c56331c3d2001b1e768d8d83a1aef0c02bf
Donald Pakkies <donaldpakkies@gmail.com>
2022-04-02 16:57:01 +0000

    feat:  update personal access tokens table

--- database/migrations/20210820161410_create_personal_access_tokens_table.js
+++ database/migrations/20210820161410_create_personal_access_tokens_table.js
@@ -5,10 +5,11 @@ exports.up = (knex) => {
 	return knex.schema.createTable('personal_access_tokens', (table) => {
 		table.increments('id').primary();
 		table.string('tokenable_type');
-		table.integer('tokenable_id');
+		table.integer('tokenable_id').index();
 		table.string('name');
 		table.string('abilities').nullable();
 		table.string('payload').nullable();
+		table.integer('ttl').index().nullable();
 		table.timestamp('last_used_at').nullable();
 		table.timestamps(true, true);
 	});
