567f53ee11ff5864383add028c9f852d931d10b5
Donald Pakkies <donaldpakkies@gmail.com>
2025-04-17 11:36:32 +0200

    feat: add factory for users table

---
+++ database/factories/UserFactory.js
@@ -0,0 +1,24 @@
+const { Factory, Hash } = require('@formidablejs/framework');
+
+exports.UserFactory = class UserFactory extends Factory {
+    constructor() {
+        super();
+        this.password = undefined;
+    }
+
+    async definition() {
+        return {
+            name: this.faker().person.fullName(),
+            email: this.faker().internet.email(),
+            email_verified_at: now(),
+            password: this.password ?? await Hash.make('password'),
+            remember_token: encrypt(strRandom(80)),
+        };
+    }
+
+    unverified() {
+        return this.state({
+            email_verified_at: null
+        });
+    }
+}
