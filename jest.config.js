5b34eddeb12ec5265c6c1e1c0c661cd19681c6a3
Donald Pakkies <donaldpakkies@gmail.com>
2025-10-21 00:20:53 +0200

    feat: add mock

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,22 @@
 module.exports = {
+    testEnvironment: 'node',
     moduleNameMapper: {
         imba: '<rootDir>/node_modules/imba/dist/imba.node.js',
     },
     moduleFileExtensions: [
         'imba',
         'js',
+        'ts',
         'json',
     ],
     testMatch: [
-        '<rootDir>/test/**/*.imba'
+        '<rootDir>/test/**/*.imba',
+        '<rootDir>/test/**/*.ts',
     ],
     transform: {
         '\\.(imba)$': '<rootDir>/node_modules/@formidablejs/framework/bin/jest/preprocessor.js',
     },
+    transformIgnorePatterns: [
+        'node_modules/(?!(.*\\.mjs$|@paralleldrive/cuid2|@formidablejs|supertest|superagent|formidable|@paralleldrive))'
+    ]
 };
