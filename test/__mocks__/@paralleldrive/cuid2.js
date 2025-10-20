6e69fdf3ea0f4432f66632410c0321725202f22b
Donald Pakkies <donaldpakkies@gmail.com>
2025-10-21 00:48:26 +0200

    fix: mock syntax

--- test/__mocks__/@paralleldrive/cuid2.js
+++ test/__mocks__/@paralleldrive/cuid2.js
@@ -1,15 +1,8 @@
-# Mock for @paralleldrive/cuid2
-# module.exports = {
-#     createId: jest.fn(() => 'mock-id'),
-#     init: jest.fn(),
-#     getConstants: jest.fn(() => ({})),
-#     isCuid: jest.fn(() => true)
-# };
-
+//Mock for @paralleldrive/cuid2
 
 module.exports = {
-	createId: jest.fn(do 'mock-id'),
-	init: jest.fn!,
-	getConstants: jest.fn(do ({})),
-	isCuid: jest.fn(do true)
-}
+    createId: jest.fn(() => 'mock-id'),
+    init: jest.fn(),
+    getConstants: jest.fn(() => ({})),
+    isCuid: jest.fn(() => true)
+};
